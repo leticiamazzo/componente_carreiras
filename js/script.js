d20c7200bfbe7896220206d4fdc6ec2a799238d4
Leticia Mazzo <leticiamazzo@gmail.com>
2018-11-15 19:48:02 -0200

    Concluí mobile first e evoluí com js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
 console.log("hello word");
+
+
+function moreInformations(){
+  document.getElementById("informations").innerHTML = "Menos Informações";
+
+  document.getElementById("icon1").innerHTML = `<img src="./img/icone.png" alt="ícone para pontuar as informações">`
+  document.getElementById("item1").innerHTML = `<p>Nível Básico</p>`
+
+  document.getElementById("icon2").innerHTML = `<img src="./img/icone.png" alt="ícone para pontuar as informações">`
+  document.getElementById("item2").innerHTML = `<p>12 horas para conclusão</p>`
+
+  document.getElementById("icon3").innerHTML = `<img src="./img/icone.png" alt="ícone para pontuar as informações">`
+  document.getElementById("item3").innerHTML = `<p>110 minutos de vídeo</p>`
+
+}
+
+// function changeText(){
+//   if (document.getElementById("informations").value == "Mais Informações") {
+//     document.getElementById("informations").value == "Mais Informações"    
+//   } else {
+//     document.getElementById("informations").value == "Menos Informações"    
+//   }
+// }
+
