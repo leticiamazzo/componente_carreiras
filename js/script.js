435c4b1f3d80efc1b2a4f0034c009de9bb50330b
Leticia Mazzo <leticiamazzo@gmail.com>
2018-11-16 02:18:36 -0200

    Concluí media queries e função js

--- js/script.js
+++ js/script.js
@@ -1,25 +1,9 @@
-console.log("hello word");
+function moreInformations() {
+  if(document.getElementById("informations").value == "Mais Informações"){
+    document.getElementById("informations").value = "Menos Informações";
+   } else {
+    document.getElementById("informations").value = "Mais Informações";
+   }
 
-
-function moreInformations(){
-  document.getElementById("informations").innerHTML = "Menos Informações";
-
-  document.getElementById("icon1").innerHTML = `<img src="./img/icone.png" alt="ícone para pontuar as informações">`
-  document.getElementById("item1").innerHTML = `<p>Nível Básico</p>`
-
-  document.getElementById("icon2").innerHTML = `<img src="./img/icone.png" alt="ícone para pontuar as informações">`
-  document.getElementById("item2").innerHTML = `<p>12 horas para conclusão</p>`
-
-  document.getElementById("icon3").innerHTML = `<img src="./img/icone.png" alt="ícone para pontuar as informações">`
-  document.getElementById("item3").innerHTML = `<p>110 minutos de vídeo</p>`
-
+  document.getElementById("show").classList.toggle("content");
 }
-
-// function changeText(){
-//   if (document.getElementById("informations").value == "Mais Informações") {
-//     document.getElementById("informations").value == "Mais Informações"    
-//   } else {
-//     document.getElementById("informations").value == "Menos Informações"    
-//   }
-// }
-
